9922eb496e71673ebf3f09cee391a497046ca6b3
joaozito <joaozito.polo@gmail.com>
2016-10-20 11:58:12 -0200

    search by membership start date

--- ang/peoplelist/PeopleListCtrl.js
+++ ang/peoplelist/PeopleListCtrl.js
@@ -3,46 +3,74 @@
   angular.module('peoplelist').config(function($routeProvider) {
       $routeProvider.when('/peoplelist', {
         controller: 'PeoplelistPeopleListCtrl',
+        controllerAs: 'vm',
         templateUrl: '~/peoplelist/PeopleListCtrl.html',
-
-        // If you need to look up data when opening the page, list it out
-        // under "resolve".
-        resolve: {
-          myContact: function(crmApi) {
-            return crmApi('Contact', 'getsingle', {
-              id: 'user_contact_id',
-              return: ['first_name', 'last_name']
-            });
-          }
-        }
       });
     }
   );
 
-  // The controller uses *injection*. This default injects a few things:
-  //   $scope -- This is the set of variables shared between JS and HTML.
-  //   crmApi, crmStatus, crmUiHelp -- These are services provided by civicrm-core.
-  //   myContact -- The current contact, defined above in config().
-  angular.module('peoplelist').controller('PeoplelistPeopleListCtrl', function($scope, crmApi, crmStatus, crmUiHelp, myContact) {
-    // The ts() and hs() functions help load strings for this module.
+  angular.module('peoplelist').controller('PeoplelistPeopleListCtrl', function($scope, $q, crmApi, crmStatus, crmUiHelp) {
+    
+    // local scope and helpful functions
+    var vm = this;
     var ts = $scope.ts = CRM.ts('peoplelist');
     var hs = $scope.hs = crmUiHelp({file: 'CRM/peoplelist/PeopleListCtrl'}); // See: templates/CRM/peoplelist/PeopleListCtrl.hlp
 
-    // We have myContact available in JS. We also want to reference it in HTML.
-    $scope.myContact = myContact;
+    // definitions
+    vm.filter = {};
+    vm.persons = [];
 
-    $scope.save = function save() {
-      return crmStatus(
-        // Status messages. For defaults, just use "{}"
-        {start: ts('Saving...'), success: ts('Saved')},
-        // The save action. Note that crmApi() returns a promise.
-        crmApi('Contact', 'create', {
-          id: myContact.id,
-          first_name: myContact.first_name,
-          last_name: myContact.last_name
-        })
-      );
-    };
+    // actions
+    vm.search = function() { search(vm.filter, vm.persons, $q, crmApi); };
+
   });
+
+  /** search persons using the filters beginDate and endDate */
+  function search(filter, persons, $q, crmApi) {
+    var queries = {};
+
+    // simplified contact list
+    queries.contacts = crmApi('Contact', 'getlist'); // [{ id, label }]
+    
+    // finds memberships by start date
+    var args = angular.extend({}, { 'start_date': asRangeExpression(filter['beginDate'], filter['endDate']) })
+    queries.memberships = crmApi('Membership', 'get', args);
+
+    // run queries
+    $q.all(queries).then(function(data) {
+      // prepare contacts map
+      var contacts = contactsMap(data.contacts);
+      console.log(contacts);
+
+      // update persons list
+      persons.splice(0, persons.length);
+      angular.forEach(data.memberships['values'], function(m) { this.push(angular.extend(m, { contact: contacts[m['contact_id']]  } )) }, persons);
+    }); 
+    
+    crmApi('Membership', 'get', {
+    }).then(function(result) {
+      angular.forEach(result['values'], function(value) { this.push(value); }, persons);
+    });
+  }
+
+  /** prepares a range expression */
+  function asRangeExpression(val1, val2) {
+    var out = undefined;
+    if(!!val1 && !!val2) {
+      out = { 'BETWEEN': [ val1, val2 ]};
+    } else if(!!val1) {
+      out = { '>=': val1 }
+    } else if(!!val2) {
+      out = { '<=': val2 }
+    }
+    return out;
+  } 
+
+  /** maps the contact list by id */
+  function contactsMap(list) {
+    var out = {};
+    angular.forEach(list, function(item) { this[item['id']] = item }, out);
+    return out;
+  }
 
 })(angular, CRM.$, CRM._);
